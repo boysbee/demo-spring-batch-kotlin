ad31332581d2d54848bb048fd93252f934d21c20
nattaporn.cha <nattaporn.cha@ascendcorp.com>
2018-06-19 00:43:37 +0700

    Add scheduler and sample.

--- src/main/kotlin/com/example/demo/configuration/BatchConfiguration.kt
+++ src/main/kotlin/com/example/demo/configuration/BatchConfiguration.kt
@@ -1,17 +1,26 @@
 package com.example.demo.configuration;
 
+import com.example.demo.item.ItemProcessor
+import com.example.demo.item.ItemReader
+import com.example.demo.item.ItemWriter
+import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 
-import com.example.demo.item.*;
 
 @Configuration
 @EnableBatchProcessing
+@EnableScheduling
 class BatchConfiguration {
 
     @Autowired
@@ -29,6 +38,9 @@ class BatchConfiguration {
     @Autowired
     lateinit var writer: ItemWriter
 
+    @Autowired
+    lateinit var jobLauncher: JobLauncher;
+
     @Bean
     fun step1() = stepBuilderFactory.get("step1")
             .tasklet { stepContribution, chunkContext ->
@@ -39,16 +51,46 @@ class BatchConfiguration {
 
     @Bean
     open fun step2() = stepBuilderFactory.get("step2")
-                .chunk<String, String>(10)
-                .reader(reader)
-                .processor(processor)
-                .writer(writer)
-                .build()
+            .chunk<String, String>(10)
+            .reader(reader)
+            .processor(processor)
+            .writer(writer)
+            .build()
 
     @Bean
-    fun job() = jobBuilderFactory.get("job1")
+    @Qualifier("scheduledJob")
+    fun job() = jobBuilderFactory.get("basicJob")
             .flow(step1())
             .next(step2())
             .end()
             .build()
+
+    // This job runs in every 5 seconds
+    @Scheduled(cron = "0/5 * * * * ?")
+    fun launch() = {
+        jobLauncher.run(job(), JobParametersBuilder()
+                .addLong("launchTime", System.currentTimeMillis())
+                .toJobParameters())
+    }
+
+    // This job runs in every 10 seconds
+    @Scheduled(fixedRate = 10000)
+    fun printMessage() {
+        try {
+            val jobParameters = JobParametersBuilder().addLong(
+                    "time", System.currentTimeMillis()).toJobParameters()
+            jobLauncher.run(job(), jobParameters)
+            println("I have been scheduled with Spring scheduler")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    @Bean
+    fun processJob() = jobBuilderFactory.get("processJob")
+            .incrementer(RunIdIncrementer())
+            .flow(step2()).end().build()
+
+
 }
